91c1cb941c3b498cd099185b54135e73ffb5950c
kiran <asifkiran245@gmail.com>
2020-02-28 13:10:42 +0500

    Speech to text fixed

--- UrduLanguage/views.py
+++ UrduLanguage/views.py
@@ -13,12 +13,17 @@ def speak(request):
     error = ""
     # get audio from the microphone
     r = sr.Recognizer()
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
     with sr.Microphone() as source:
-        print("Speak:")
-        audio = r.listen(source)
-
+        print("Going to Listen")
+        audio = r.listen(source, timeout=1, phrase_time_limit=10)
+        print("Listening Finished")
     try:
+        print("I'm in try")
+        # output = "" + r.adjust_for_ambient_noise(audio, duration = 1)
         output = " " + r.recognize_google(audio)
+        # output = r.adjust_for_ambient_noise(audio, duration=5)
     except sr.UnknownValueError:
         error = "Couldn't hear anything. Please speak a little loud and clear"
     except sr.RequestError as e:
